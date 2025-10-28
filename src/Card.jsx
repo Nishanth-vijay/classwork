37abf7985cb6cfa4689cefee3822f8b891eac47d
Nishanth-vijay <nishanthturpati@gmail.com>
2025-10-28 18:22:14 +0530

    firstcomit

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,31 +1,31 @@
-import data from'./assets/db.json'
+import data from './assets/db.json'
 
-import './card.css'
-export default function Card(){
-    return(
-        <div id = "card">
-{
+import './Card.css'
+export default function Card() {
+    return (
+        <div id="card">
+            {
 
-data.map(
+                data.map(
 
-(item)=>(
-<div key={item.id}>
-<img src={item.image} alt="Image" />
-<h1>{item.title}</h1>
-<center>
-<h1>${item.price}</h1>
-<h2>{item.category}</h2>
-</center>
-    </div>
-
-
-
-)
-
-)
+                    (item) => (
+                        <div key={item.id}>
+                            <img src={item.image} alt="Image" />
+                            <h1>{item.title}</h1>
+                            <center>
+                                <h1>${item.price}</h1>
+                                <h2>{item.category}</h2>
+                            </center>
+                        </div>
 
 
-}
+
+                    )
+
+                )
+
+
+            }
 
 
 
